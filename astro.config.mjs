1aab25296a221efe22e2f803705a7c1802270ede
TheNewLad <22102599+TheNewLad@users.noreply.github.com>
2024-03-31 12:21:40 -0500

    fix(url): replace newlad with thenewlad

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://newlad.com",
+  site: "https://thenewlad.com",
   integrations: [mdx(), sitemap(), tailwind()],
 });
