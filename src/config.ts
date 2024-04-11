14223582457d28d7069a1d99c6adf341f11d424a
TheNewLad <22102599+TheNewLad@users.noreply.github.com>
2024-04-10 21:17:26 -0500

    refactor(theme): remove old theme

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "theNewLad | Personal Blog and Portfolio";
-export const SITE_DESCRIPTION =
-  "theNewLad is a personal blog and portfolio website of a software engineer and tech enthusiast. Here you can fine out about my projects, blogs and other stuff.";
-export const GENERATE_SLUG_FROM_TITLE = true;
-export const TRANSITION_API = true;
